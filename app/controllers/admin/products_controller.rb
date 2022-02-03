0173530d06af17e61bd740683c469ffc02d743c8
Ahmed Elmolla <aelmollaz@gmail.com>
2022-02-03 15:15:25 +0000

    Added Admin pages authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_USER'],
+                               password: ENV['HTTP_BASIC_PASSWORD'],
+                               if: -> { ENV['HTTP_BASIC_PASSWORD'].present? }
   def index
     @products = Product.order(id: :desc).all
   end
